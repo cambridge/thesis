80d20ff5349f0e266bc775244b6ffbc88db3f691
Matej Urbas <matej.urbas@gmail.com>
2016-09-04 14:59:08 +0100

    Extracted BinTray upload function.

--- .travis.script.sh
+++ .travis.script.sh
@@ -6,17 +6,24 @@ function getVersion {
   [[ $TRAVIS_PULL_REQUEST -eq 'false' ]] && echo -n $TRAVIS_BRANCH || echo -n $TRAVIS_PULL_REQUEST.$TRAVIS_BUILD_NUMBER.$(date +%Y-%m-%d.%H%M%S).$TRAVIS_COMMIT;
 }
 
+function uploadFileToBinTray {
+  local fileToUpload=$1;
+  local remoteArtifactId=$2;
+  local version=$3;
+  local fakeVersion='1.0.1'
+  curl -X DELETE -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/cam-thesis/$fakeVersion/$remoteArtifactId-$version.pdf
+  curl -X PUT -T $fileToUpload -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/cam-thesis/$fakeVersion/$remoteArtifactId-$version.pdf?publish=1  
+}
+
 export BUILD_VERSION=$(getVersion)
 
 make
 
-curl -X DELETE -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/cam-thesis/1.0.0/thesis-$BUILD_VERSION.pdf
-curl -X PUT -T thesis.pdf -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/cam-thesis/1.0.0/thesis-$BUILD_VERSION.pdf?publish=1
+uploadFileToBinTray thesis.pdf thesis $BUILD_VERSION
 
 cd Samples/clean
 ln -s ../../CollegeShields .
 ln -s ../../cam-thesis.cls .
 ln -s ../../Makefile .
 make
-curl -X DELETE -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/cam-thesis/1.0.0/thesis-$BUILD_VERSION.pdf
-curl -X PUT -T thesis.pdf -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/cam-thesis/1.0.0/SampleClean-$BUILD_VERSION.pdf?publish=1
+uploadFileToBinTray thesis.pdf SampleClean $BUILD_VERSION
