92d7835588070b1c6a03f455b8062b283e9660fb
Matej Urbas <matej.urbas@gmail.com>
2016-09-04 15:03:39 +0100

    fixmeup

--- .travis.script.sh
+++ .travis.script.sh
@@ -3,19 +3,32 @@
 set -e;
 
 function getVersion {
-  [[ $TRAVIS_PULL_REQUEST -eq 'false' ]] && echo -n $TRAVIS_BRANCH || echo -n $TRAVIS_PULL_REQUEST.$TRAVIS_BUILD_NUMBER.$(date +%Y-%m-%d.%H%M%S).$TRAVIS_COMMIT;
+  if [[ $TRAVIS_PULL_REQUEST -ge 1 ]]; then
+    echo -n 0.0.$TRAVIS_BUILD_NUMBER-pr$TRAVIS_PULL_REQUEST;
+  elif [[ $TRAVIS_BRANCH -eq 'feature/travis-multi-builds' ]]; then
+    echo -n 0.0.$TRAVIS_BUILD_NUMBER;
+  else
+    echo -n 0.0.$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT;
+  fi
+  # else
+  #   echo -n $TRAVIS_PULL_REQUEST.$TRAVIS_BUILD_NUMBER.$(date +%Y-%m-%d.%H%M%S).$TRAVIS_COMMIT;
+  # fi
 }
 
 function uploadFileToBinTray {
   local fileToUpload=$1;
   local remoteArtifactId=$2;
   local version=$3;
-  local fakeVersion='1.0.1'
-  curl -X DELETE -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/cam-thesis/$fakeVersion/$remoteArtifactId-$version.pdf
-  curl -X PUT -T $fileToUpload -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/cam-thesis/$fakeVersion/$remoteArtifactId-$version.pdf?publish=1  
+  curl -X DELETE -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/cam-thesis/$version/$remoteArtifactId-$version.pdf
+  curl -X PUT -T $fileToUpload -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/cam-thesis/$version/$remoteArtifactId-$version.pdf?publish=1  
 }
 
 export BUILD_VERSION=$(getVersion)
+
+echo "Branch: $TRAVIS_BRANCH";
+echo "Version: $BUILD_VERSION";
+
+exit 1;
 
 make
 
