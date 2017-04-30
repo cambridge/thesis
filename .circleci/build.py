4ba6efe91ed3c7b36a600fab82e9a69585dbe850
Matej Urbas <matej.urbas@gmail.com>
2017-04-30 11:21:16 +0100

    fixmeup

--- .circleci/build.py
+++ .circleci/build.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import logging
-from os import listdir, environ
-from os.path import join
+from os.path import join, basename, isfile
 from subprocess import check_call
 
+from os import listdir, environ
 
 logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
 
@@ -19,8 +19,11 @@ def main():
     # _copy_sources_to_build_dir(_samples_source_dir)
     # _copy_sources_to_build_dir(_tests_dir)
     # _build()
-    if environ['CIRCLECI'] == 'true':
-        logging.info("RUNNING IN CIRCLECI")
+    if _is_running_in_circle_ci():
+        # if environ['CIRCLE_BRANCH'] == 'master':
+        for pdf in listdir(_pdfs_dir):
+            if isfile(pdf):
+                _upload_file_to_bintray(pdf)
 
 
 def _clean_build_dir():
@@ -39,11 +42,13 @@ def _build():
     check_call(['mkdir', '-p', (_pdfs_dir)])
     for sample_name in listdir(_samples_build_dir):
         logging.info("Building '{}'...".format(sample_name))
+
         sample_build_dir = join(_samples_build_dir, sample_name)
         check_call(['sh', '-c', '(cd {} && make)'.format(sample_build_dir)])
         check_call(['cp',
                     '{}/thesis.pdf'.format(sample_build_dir),
                     '{}/{}.pdf'.format(_pdfs_dir, sample_name)])
+
         logging.info("Successfully built '{}'.".format(sample_name))
 
 
@@ -56,6 +61,43 @@ def _copy_main_thesis_to_build():
 def _copy_base_files_to(target_dir):
     check_call(['mkdir', '-p', target_dir])
     check_call(['cp', '-r', 'cam-thesis.cls', 'Makefile', 'CollegeShields', target_dir])
+
+
+def _is_running_in_circle_ci():
+    return environ.get('CIRCLECI', 'false') == 'true'
+
+
+def _upload_file_to_bintray(file_path, package_name=None, artifact_name=None, version=None):
+    package_name = package_name or environ['CIRCLE_BRANCH']
+    version = version or environ['CIRCLE_BUILD_NUM'] + '-' + environ['CIRCLE_SHA1'][:8]
+    artifact_name = artifact_name or '{}-{}.pdf'.format(basename(file_path), version)
+
+    logging.info("Uploading '%s' to BinTray...", file_path)
+
+    bintray_credentials = '-umatej:' + environ['BINTRAY_API_KEY']
+    check_call(['curl', '-X', 'DELETE', bintray_credentials,
+                'https://api.bintray.com/content/matej/cam-thesis/{}/{}/{}'
+               .format(package_name, version, artifact_name)])
+    check_call(['curl', '-X', 'PUT', '-T', file_path, bintray_credentials,
+                'https://api.bintray.com/content/matej/cam-thesis/{};bt_package={};bt_version={};publish=1;override=1'
+               .format(artifact_name, package_name, version)])
+
+    logging.info("Uploaded to 'https://bintray.com/matej/cam-thesis/download_file?file_path=%s'.", artifact_name)
+
+
+# function uploadFileToBinTray {
+#   local fileToUpload=$1;
+#   local packageName=$2;
+#   local remoteArtifactName=$3;
+#   local version=$4;
+#   logInfo "Uploading: https://api.bintray.com/content/matej/cam-thesis/$packageName/$version/$remoteArtifactName";
+#
+#   curl -X DELETE -umatej:$BINTRAY_API_KEY https://api.bintray.com/content/matej/cam-thesis/$packageName/$version/$remoteArtifactName;
+#   curl -X PUT -T $fileToUpload -umatej:$BINTRAY_API_KEY "https://api.bintray.com/content/matej/cam-thesis/$remoteArtifactName;bt_package=$packageName;bt_version=$version;publish=1;override=1";
+#
+#   echo "";
+#   logSuccess "Uploaded to: https://bintray.com/matej/cam-thesis/download_file?file_path=$remoteArtifactName";
+# }
 
 
 if __name__ == '__main__':
